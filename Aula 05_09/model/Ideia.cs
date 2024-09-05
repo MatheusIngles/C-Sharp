7e4e886aaa5ee0937790548c01a6878699aa5aed
Matheus Endlich Silveira <126157948+MatheusIngles@users.noreply.github.com>
2024-09-05 08:47:50 -0300

    Add files via upload

---
+++ Aula 05_09/model/Ideia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.percistencia;
+
+namespace WpfApp1.model
+{
+    internal class Ideia
+    {
+        public int Custo { get; set; }
+        public string Area { get; set; }
+        public string idea { get; set; }
+        public string Nivel { get; set; }
+
+        public Boolean CadastarIdeia(Ideia idea) { 
+            BD.SalvarBD(idea);
+            return true;
+        }
+    }
+}
---
+++ Aula 05_09/percistencia/BD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.model;
+
+namespace WpfApp1.percistencia
+{
+    internal class BD
+    {
+      public static List<Ideia> list = [];
+
+      public static void SalvarBD(Ideia ideia) => list.Add(ideia);
+
+      public static List<Ideia> RetornarBd() => list;
+    }
+}
